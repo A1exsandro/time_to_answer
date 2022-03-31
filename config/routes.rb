06efa5b8d377d1d2f65ea1c8d651ee94af41a27f
A1exsandro <alexsandro.acp@gmail.com>
2022-03-31 16:53:59 -0400

    feat: create logins with devise
    Add Devise and create two views of logins: admin and user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_for :admins
+  get 'welcome/index'
+
+  root to: 'welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
