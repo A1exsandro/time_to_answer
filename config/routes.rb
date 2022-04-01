6da3d2a0ec38f4dcd24f9be52c7b85b503905250
A1exsandro <alexsandro.acp@gmail.com>
2022-04-01 14:09:23 -0400

    feat: create task for register faker admins

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,10 @@ Rails.application.routes.draw do
     get 'welcome/index'
   end
   namespace :admins_backoffice do
+    get 'admins/index'
     get 'welcome/index'
   end
-  devise_for :users
+  devise_for :users 
   devise_for :admins
   #get 'welcome/index'
 
