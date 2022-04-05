bfcc4c2920e257cddd0e5905cc1dd9268584624b
A1exsandro <alexsandro.acp@gmail.com>
2022-04-05 01:01:42 -0400

    refactor: good practices for mvc

--- app/controllers/admins_backoffice/questions_controller.rb
+++ app/controllers/admins_backoffice/questions_controller.rb
@@ -1,54 +1,59 @@
 class AdminsBackoffice::QuestionsController < AdminsBackofficeController
-    before_action :set_question, only: [:edit, :update, :destroy]
+  before_action :set_question, only: [:edit, :update, :destroy]
+  before_action :get_subjects, only: [:new, :edit]
     
   
-    def index
-      @questions = Question.all.order(:description).page(params[:page]).per(5)
-    end
+  def index
+    @questions = Question.all.order(:description).page(params[:page]).per(5)
+  end
   
-    def new 
-      @question = Question.new
-    end
+  def new 
+    @question = Question.new
+  end
   
-    def create
-      @question = Question.new(params_question)
-      if @question.save
-        redirect_to admins_backoffice_questions_path, notice: "Questão cadastrada com sucesso!"
-      else
+  def create
+    @question = Question.new(params_question)
+    if @question.save
+      redirect_to admins_backoffice_questions_path, notice: "Questão cadastrada com sucesso!"
+    else
         render :new
-      end
     end
+  end
   
-    def edit
+  def edit
+  end
+  
+  def update
+    if @question.update(params_question)
+      redirect_to admins_backoffice_questions_path, notice: "Questão atualizada com sucesso!"
+    else
+      render :edit
     end
+  end
   
-    def update
-      if @question.update(params_question)
-        redirect_to admins_backoffice_questions_path, notice: "Questão atualizada com sucesso!"
-      else
-        render :edit
-      end
-    end
-  
-    def destroy
-      if @question.destroy(params_question)
-        redirect_to admins_backoffice_questions_path, notice: "Questão excluída com sucesso!"
-      else
-        render :index
-      end
-    end
-  
-    def show 
+  def destroy
+    if @question.destroy(params_question)
       redirect_to admins_backoffice_questions_path, notice: "Questão excluída com sucesso!"
+    else
+      render :index
     end
+  end
   
-    private
+  def show 
+    redirect_to admins_backoffice_questions_path, notice: "Questão excluída com sucesso!"
+  end
   
-    def params_question
-      params.require(:question).permit(:description, :subject_id)
-    end
+  private
   
-    def set_question 
-      @question = Question.find(params[:id])
-    end
+  def params_question
+    params.require(:question).permit(:description, :subject_id)
+  end
+  
+  def set_question 
+    @question = Question.find(params[:id])
+  end
+
+  def get_subjects
+    @subjects = Subject.all
+  end
 end
